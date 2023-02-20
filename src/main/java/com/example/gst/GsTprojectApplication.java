97f31549162e2d147f58532c5b6ef177d5e842d4
MayssaHkiri <may26saa@gmail.com>
2023-02-20 10:59:32 +0100

    first commit by mayssa

---
+++ src/main/java/com/example/gst/GsTprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsTprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsTprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gst/GsTprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsTprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
