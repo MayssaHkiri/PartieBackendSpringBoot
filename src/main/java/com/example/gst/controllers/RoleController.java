545ed9c2283e2fe29854a3ba0c1ae6a7684b27ed
MayssaHkiri <may26saa@gmail.com>
2023-02-20 18:01:39 +0100

    role controller

--- src/main/java/com/example/gst/controllers/RoleController.java
+++ src/main/java/com/example/gst/controllers/RoleController.java
@@ -1,8 +1,5 @@
 package com.example.gst.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 public class RoleController {
 
