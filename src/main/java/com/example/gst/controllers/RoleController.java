69d0360a3fa21ac493cb3c18bac5244bad1be139
MayssaHkiri <may26saa@gmail.com>
2023-02-20 17:53:02 +0100

    qlq modif

---
+++ src/main/java/com/example/gst/controllers/RoleController.java
@@ -0,0 +1,9 @@
+package com.example.gst.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class RoleController {
+
+}
--- src/main/java/com/example/gst/controllers/UtilisateurController.java
+++ src/main/java/com/example/gst/controllers/UtilisateurController.java
@@ -24,7 +24,7 @@ public class UtilisateurController {
 	
 	@Autowired 
 	private UtilisateurService userService ; 
-	@PostMapping()
+	@PostMapping("/ajouter")
 	public Utilisateur AjouterUtilisateur (@RequestBody Utilisateur user ) {
 		return userService.AjouterUtilisateur(user);
 	}
