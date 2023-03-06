93f1ee022950c8d877a75e9ab51902e112d59852
MayssaHkiri <may26saa@gmail.com>
2023-03-06 22:18:00 +0100

    loginRequest and SignupRequest models

---
+++ src/main/java/com/example/gst/models/SignupRequest.java
@@ -0,0 +1,28 @@
+package com.example.gst.models;
+
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class SignupRequest {
+	@NotBlank
+	@Size(min = 3, max = 20)
+	private String firstName;
+
+	@NotBlank
+	@Size(min = 3, max = 20)
+	private String lastName;
+
+	@NotBlank
+	@Size(max = 50)
+	@Email
+	private String email;
+
+	@NotBlank
+	@Size(min = 6, max = 40)
+	private String password;
+	private Set<String> role;
+	
+}
