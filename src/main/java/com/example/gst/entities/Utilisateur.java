9c8cf5e9d311a3838f366d740a4bb4be17f381f1
khouloud <khouloudmhamdi7@gmail.com>
2023-03-10 17:40:29 +0100

    changes

--- src/main/java/com/example/gst/entities/Utilisateur.java
+++ src/main/java/com/example/gst/entities/Utilisateur.java
@@ -1,19 +1,19 @@
 package com.example.gst.entities;
 
-import java.util.List;
+
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ForeignKey;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity 
 @Table(name="UTILISATEUR") 
@@ -33,7 +33,7 @@ public class Utilisateur {
 	
 	@ManyToOne
 	@JoinColumn(name="ID_ROLE", referencedColumnName="ID_ROLE")
-	@ForeignKey(name = "FK_USER_ROLE", foreignKeyDefinition = "FOREIGN KEY (ID_ROLE) REFERENCES role(ID_Role)")
+	
 	private Role role ;
 	
 	@ManyToOne
